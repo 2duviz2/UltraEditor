f2bea98ef803d0a24b10e05182dbd9fddde0197f
Duviz 2 <javierduviz2@gmail.com>
2025-10-27 13:17:09 +0100

    Remade assetItem

---
+++ UltraEditor/Classes/AssetItem.cs
@@ -0,0 +1,31 @@
+﻿using Steamworks.Ugc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UltraEditor.Classes
+{
+    public class AssetItem : MonoBehaviour
+    {
+        public string assetPath;
+        public string assetName;
+
+        public TMP_Text assetNameText;
+        public GameObject assetItemObject;
+
+        public void Start()
+        {
+            if (assetPath == "")
+                assetPath = assetItemObject.name;
+            else
+                assetNameText.text = assetName;
+            GetComponent<Button>().onClick.AddListener(() =>
+            {
+                EditorManager.Instance.SpawnAsset(assetPath);
+            });
+        }
+    }
+}
