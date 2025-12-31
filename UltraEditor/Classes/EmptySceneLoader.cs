f4486798ba8846571a4ecc2654d04c827fd9c2a0
Bryan_-000- <fredayddd321ewq@gmail.com>
2025-12-31 14:08:09 +0000

    why dont you just delete these?

--- UltraEditor/Classes/EmptySceneLoader.cs
+++ UltraEditor/Classes/EmptySceneLoader.cs
@@ -56,9 +56,6 @@ public class EmptySceneLoader : MonoBehaviour
         DontDestroyOnLoad((Instance = this).gameObject);
 
         Addressables.LoadAssetAsync<GameObject>("FirstRoom").WaitForCompletion();
-        /*Addressables.LoadAssetAsync<GameObject>("FirstRoom Secret").WaitForCompletion(); not planned to be added yet
-        Addressables.LoadAssetAsync<GameObject>("FirstRoom Prime").WaitForCompletion();
-        Addressables.LoadAssetAsync<GameObject>("Assets/Prefabs/Levels/Special Rooms/FirstRoom Encore.prefab").WaitForCompletion();*/
 
         Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("UltraEditor.Assets.emptyscene.bundle");
 
