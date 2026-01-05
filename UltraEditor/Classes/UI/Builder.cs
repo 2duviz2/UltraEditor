c0da99d6ddcf8ee4bfe7964f37054ed4e2a4df1d
Bryan_-000- <fredayddd321ewq@gmail.com>
2026-01-05 20:07:14 +0000

    ah shit i forgot to commit image, alright heres image and button together

--- UltraEditor/Classes/UI/Builder.cs
+++ UltraEditor/Classes/UI/Builder.cs
@@ -20,4 +20,35 @@ public static class Builder
 
         return ComponentT;
     }
+
+    /// <summary> Creates an Image with the given sprite and color. </summary>
+    public static Image Image(string name, Transform parent, Rect rect, Sprite sprite = null, Color? col = null) =>
+        Manufacture<Image>(name, parent, rect, image =>
+        {
+            image.type = UnityEngine.UI.Image.Type.Sliced;
+            image.sprite = sprite ?? Sprites.Fill;
+            image.color = col ?? Color.white;
+
+            if (Sprites.SpriteVals.TryGetValue(sprite ?? Sprites.Fill, out var val)) 
+                image.pixelsPerUnitMultiplier = val.ppu;
+            else 
+                image.pixelsPerUnitMultiplier = 4.05f;
+        });
+
+    /// <summary> Creates a Button with the given sprite, color, and onClick Action. </summary>
+    public static Button Button(string name, Transform parent, Rect rect, Sprite sprite = null, Color? col = null, Action onClick = null)
+    {
+        Image img = Image(name, parent, rect, sprite, col);
+        Button button = img.gameObject.AddComponent<Button>();
+
+        if (Sprites.SpriteVals.TryGetValue(sprite ?? Sprites.Fill, out var val))
+            button.colors = val.cb;
+        else
+            button.colors = Sprites.FillColor;
+
+        button.targetGraphic = img;
+        button.onClick.AddListener(() => onClick());
+
+        return button;
+    }
 }
