b35f18780a2bcb12eb613ffcae4b80d4bc1baf65
Bryan_-000- <fredayddd321ewq@gmail.com>
2026-02-13 22:36:54 +0000

    welp uh like add folder ur in and viewing non-foldered assets in the main assets folder like rude

--- UltraEditor/Classes/AssetsWindowManager.cs
+++ UltraEditor/Classes/AssetsWindowManager.cs
@@ -3,26 +3,46 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using TMPro;
 using UltraEditor.Libraries;
 using UnityEngine;
 
 /// <summary> Manages/Handles everything for the assets window. </summary>
-public class AssetsWindowManager : MonoSingleton<AssetsWindowManager>
+public class AssetsWindowManager : MonoBehaviour
 {
+    public static AssetsWindowManager Instance;
+
     /// <summary> Hidden AssetItem template. </summary>
     public AssetItem Template;
 
     /// <summary> Hidden AssetFolder template. </summary>
     public AssetFolder FolderTemplate;
 
+    /// <summary> The text that says the path to the currently open folder in the assets window. </summary>
+    public TextMeshProUGUI AssetsFolderPathText;
+
     /// <summary> The currently open folder. </summary>
     [HideInInspector]
     public string CurrentFolder;
+
+    /// <summary> Assign the Instance. </summary>
+    public void Awake() =>
+        Instance = this;
 
     /// <summary> Setup the window. </summary>
     public void Start()
     {
         CurrentFolder = "Assets/"; // start in the assets folder
+        Refresh();
+    }
+
+    /// <summary> Goes back to the previous folder in the path. </summary>
+    public void PreviousFolder()
+    {
+        int end = CurrentFolder[..^1].LastIndexOf('/') + 1;
+        if (end == 0) return;
+
+        CurrentFolder = CurrentFolder[..end];
         Refresh();
     }
 
@@ -41,7 +61,7 @@ public class AssetsWindowManager : MonoSingleton<AssetsWindowManager>
         {
             AssetFolder newAssetFolder = Instantiate(FolderTemplate, transform);
             newAssetFolder.folderPath = folder;
-            newAssetFolder.folderName = folder[folder[..^1].LastIndexOf('/')..^1]; // this just gets the name of the folder it looks scary ik its cuz im bad at programming
+            newAssetFolder.folderName = folder[(folder[..^1].LastIndexOf('/')+1)..^1]; // this just gets the name of the folder it looks scary ik its cuz im bad at programming
 
             newAssetFolder.gameObject.SetActive(true);
         }
@@ -55,6 +75,8 @@ public class AssetsWindowManager : MonoSingleton<AssetsWindowManager>
 
             newAssetItem.gameObject.SetActive(true);
         }
+
+        AssetsFolderPathText.text = CurrentFolder;
     }
 
     /// <summary> A dictionary of folder paths and a list of all the key's of the assets inside that folder. </summary>
@@ -64,15 +86,26 @@ public class AssetsWindowManager : MonoSingleton<AssetsWindowManager>
     public static void LoadFolders()
     {
         List<string> keys = AssHelper.GetPrefabAddressableKeys();
-        List<string> GetAssetsInFolder(string folder) =>
-            [.. keys.Where(key => key.StartsWith(folder) && !key[folder.Length..].Contains('/'))];
 
-        Folders.Add("Assets/", []);
         foreach (string key in keys)
         {
             string folder = Path.GetDirectoryName(key).Replace('\\', '/') + "/";
             if (!Folders.ContainsKey(folder))
+            {
                 Folders.Add(folder, GetAssetsInFolder(folder));
+
+                foreach (int index in folder[..^1].Occurences('/'))
+                {
+                    string subFolder = folder[..(index + 1)];
+
+                    if (!Folders.ContainsKey(subFolder))
+                        Folders.TryAdd(subFolder, GetAssetsInFolder(subFolder));
+                }
+            }
         }
+
+        List<string> GetAssetsInFolder(string folder) =>
+            [.. keys.Where(key => key.StartsWith(folder) && !key[folder.Length..].Contains('/'))
+                .Concat(folder == "Assets/" ? GetAssetsInFolder("") : [])]; // this is just for the assets folder since theres assets with no folder :P
     }
 }
