bf1783503e9eb9209faeb1e4b7e554d835001dc7
Bryan_-000- <fredayddd321ewq@gmail.com>
2026-03-02 18:39:38 +0000

    give portals a silly lil outline :3333

--- UltraEditor/Classes/IO/SaveObjects/PortalObject.cs
+++ UltraEditor/Classes/IO/SaveObjects/PortalObject.cs
@@ -1,5 +1,6 @@
 ﻿namespace UltraEditor.Classes.IO.SaveObjects;
 
+using System.Collections.Generic;
 using ULTRAKILL.Portal;
 using ULTRAKILL.Portal.Geometry;
 using UnityEngine;
@@ -21,7 +22,7 @@ public class PortalObject : SavableObject
 
     /// <summary> The exit portal. </summary>
     public GameObject PortalExit;
-    
+
     /// <summary> Portal component mrrrp miaow </summary>
     public Portal portal;
 
@@ -75,7 +76,60 @@ public class PortalObject : SavableObject
     public override void Tick()
     {
         if (portal.shape is PlaneShape shape && (shape.width != PortalWidth || shape.height != PortalHeight))
-            portal.shape = new PlaneShape() 
-                { width = PortalWidth, height = PortalHeight };
+            portal.shape = new PlaneShape() { width = PortalWidth, height = PortalHeight };
+    }
+
+    /// <summary> Line material for drawing the outline. </summary>
+    public Material LineMat;
+
+    /// <summary> Render the outline for the portal if we have the editor open. </summary>
+    public void OnRenderObject()
+    {
+        if (!EditorManager.Instance.editorOpen)
+            return;
+
+        // portals width and height are divided by 5 :3
+        float width =  (PortalWidth /5f) / 2f;
+        float height = (PortalHeight/5f) / 2f;
+
+        DrawOutline(PortalEntrance.transform);
+        DrawOutline(PortalExit.transform);
+
+        void DrawOutline(Transform local)
+        {
+            if (!(LineMat ??= new(DefaultReferenceManager.Instance.masterShader)).SetPass(0))
+                return;
+
+            GL.PushMatrix();
+            GL.MultMatrix(local.localToWorldMatrix);
+
+            DrawLines(new(0f, 1f, 0.75f),
+            [
+                new(width, height, 0),
+                new(width, -height, 0),
+                
+                new(-width, -height, 0),
+                new(width, -height, 0),
+                
+                new(-width, -height, 0),
+                new(-width, height, 0),
+
+                new(-width, height, 0),
+                new(width, height, 0),
+            ]);
+
+            GL.PopMatrix();
+        }
+    }
+
+    /// <summary> Draws a buncha lines like a gizmo :sleepingfilth: </summary>
+    public void DrawLines(Color col, params List<Vector3> linePositions)
+    {
+        GL.Begin(GL.LINES);
+        GL.Color(col);
+
+        linePositions.ForEach(GL.Vertex);
+
+        GL.End();
     }
 }
